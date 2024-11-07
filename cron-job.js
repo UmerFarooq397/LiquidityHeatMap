6af96065c1b5a029af40468ec3d9e7f88c01d206
harismajeed <hm79405@gmail.com>
2024-11-08 02:11:16 +0500

    wallet cron job

--- cron-job.js
+++ cron-job.js
@@ -2,32 +2,30 @@
 const cron = require('node-cron');
 const { fetchAndBroadcastTradeData, broadcastMoonPhaseAndSignals, checkProfitableBotWallets } = require('./algo'); 
 
-const symbols = ['BTCUSDT', 'ETHUSDT', 'SOLUSDT', 'AVAXUSDT', 'SUIUSDT', 'SEIUSDT', 'APTUSDT', 'OPUSDT', 'INJUSDT', 'ARBUSDT', 'FETUSDT']; 
+// const symbols = ['BTCUSDT', 'ETHUSDT', 'SOLUSDT', 'AVAXUSDT', 'SUIUSDT', 'SEIUSDT', 'APTUSDT', 'OPUSDT', 'INJUSDT', 'ARBUSDT', 'FETUSDT']; 
+const symbols = ['BTCUSDT']; 
 
 
 const { connectDB } = require('./db');
 
-
 connectDB().then(() => {
-    checkProfitableBotWallets();
-    //startProfitableWalletJobs();
-    // startTradeDataCronJobs();  
-    //startMoonPhaseCronJobs();
+    startProfitableWalletJobs();
+    startTradeDataCronJobs();  
+    startMoonPhaseCronJobs();
 }).catch(error => {
   console.error('Failed to connect to MongoDB:', error);
   process.exit(1); 
 });
 
 function startTradeDataCronJobs() {
-    /** Schedule the cron job to run every 60 minutes */
-    cron.schedule('* * * * *', async () => {
+    /** Schedule the cron job to run every day */
+    cron.schedule('0 0 * * *', async () => {
         console.log(`Running fetchAndBroadcastTradeData for symbols at ${new Date()}`);
     
         for (const symbol of symbols) {
             try {
                 console.log(`Fetching and broadcasting trade data for symbol: ${symbol}`);
-                //await fetchAndBroadcastTradeData(symbol);
-                await     checkProfitableBotWallets();
+                await fetchAndBroadcastTradeData(symbol);
                 console.log(`Successfully executed fetchAndBroadcastTradeData for ${symbol}`);
             } catch (error) {
                 console.error(`Error executing fetchAndBroadcastTradeData for ${symbol}:`, error);
@@ -37,8 +35,8 @@ function startTradeDataCronJobs() {
 }
 
 function startMoonPhaseCronJobs() {
-    /** Schedule the cron job to run every 60 minutes */
-    cron.schedule('0 * * * *', async () => {
+    /** Schedule the cron job to run every day */
+    cron.schedule('0 0 * * *', async () => {
         console.log(`Running fetchAndBroadcastTradeData for symbols at ${new Date()}`);
     
         for (const symbol of symbols) {
@@ -54,7 +52,7 @@ function startMoonPhaseCronJobs() {
 }
 
 function startProfitableWalletJobs() {
-    /** Schedule the cron job to run every 60 minutes */
+    /** Schedule the cron job to run every day */
     cron.schedule('0 0 * * *', async () => {
         console.log(`Running checkProfitableBotWallets for symbols at ${new Date()}`);
     
